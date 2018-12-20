4c773ac3f08c581a6767b1d2a0760123baa2a2a2
joniff <joniff@hotmail.com>
2018-12-20 01:35:31 +0000

    First run thru working

--- src/uSync.MemberEdition/Serializers/MemberHandler.cs
+++ src/uSync.MemberEdition/Serializers/MemberHandler.cs
@@ -14,40 +14,42 @@ using Umbraco.Core.Models;
 using Umbraco.Core.Persistence;
 using Umbraco.Core.Services;
 using uSync.MemberEdition.Rdbms;
+using uSync.MemberEdition.Security;
+using uSync.MemberEdition.Xml;
 
 namespace uSync.MemberEdition.Serializers
 {
 	public class MemberHandler : ISyncHandler
 	{
 		public string Name { get { return "uSync: MemberHandler"; } }
-        public int Priority { get { return Jumoo.uSync.BackOffice.uSyncConstants.Priority.Content + 1; } }
-        public string SyncFolder { get { return "Member"; } }
+		public int Priority { get { return Jumoo.uSync.BackOffice.uSyncConstants.Priority.Content + 1; } }
+		public string SyncFolder { get { return "Member"; } }
 
 		private IMemberService _memberService;
 		private IMemberTypeService _memberTypeService;
 		private IMemberGroupService _memberGroupService;
 		private IRuntimeCacheProvider _runtimeCacheProvider;
 		private UmbracoDatabase Database;
-		
-		private const string NodeName = "member";
-		private const string KeyAttribute = "email";
-		private const string TypeAttribute = "type";
-		private const string AliasAttribute = "alias";
-		private const string NameAttribute = "name";
-		private const string UserAttribute = "user";
 
-		private const string CommentsNode = "comments";
-		private const string FailedPasswordAttemptsNode = "failedPasswordAttempts";
-		private const string GroupsNode = "groups";
-		private const string GroupNode = "group";
-		private const string IsApprovedNode = "isApproved";
-		private const string IsLockedOutNode = "isLockedOut";
-		private const string LastLockedOutDateNode = "lastLockedOutDate";
-		private const string LastLoginDateNode = "lastLoginDate";
-		private const string LastPasswordChangeDateNode = "lastPasswordChangeDate";
-		private const string PasswordQuestionNode = "passwordQuestion";
-		private const string PasswordNode = "password";
-		private const string RawPasswordAnswerNode = "rawPasswordAnswer";
+		private const string NodeName = "Member";
+		private const string KeyAttribute = "Email";
+		private const string TypeAttribute = "Type";
+		private const string AliasAttribute = "Alias";
+		private const string NameAttribute = "Name";
+		private const string UserAttribute = "User";
+
+		private const string CommentsNode = "Comments";
+		private const string FailedPasswordAttemptsNode = "FailedPasswordAttempts";
+		private const string GroupsNode = "Groups";
+		private const string GroupNode = "Group";
+		private const string IsApprovedNode = "IsApproved";
+		private const string IsLockedOutNode = "IsLockedOut";
+		private const string LastLockedOutDateNode = "LastLockedOutDate";
+		private const string LastLoginDateNode = "LastLoginDate";
+		private const string LastPasswordChangeDateNode = "LastPasswordChangeDate";
+		private const string PasswordQuestionNode = "PasswordQuestion";
+		private const string PasswordNode = "Password";
+		private const string RawPasswordAnswerNode = "RawPasswordAnswer";
 
 		private const string MemberTypeCacheKey = "544dd2b0-11d1-41b4-a346-3435dd2d80cf:";
 		private const string FileExtension = "config";
@@ -62,7 +64,7 @@ namespace uSync.MemberEdition.Serializers
 			Database = ApplicationContext.Current.DatabaseContext.Database;
 		}
 
-		private IMemberType MemberType(string key) => _runtimeCacheProvider.GetCacheItem<IMemberType>(MemberTypeCacheKey + key.ToLowerInvariant(), () => 
+		private IMemberType MemberType(string key) => _runtimeCacheProvider.GetCacheItem<IMemberType>(MemberTypeCacheKey + key.ToLowerInvariant(), () =>
 		{
 			var member = _memberTypeService.GetAll().FirstOrDefault(x => string.Compare(x.Alias, key, true) == 0);
 			if (member != null)
@@ -70,24 +72,27 @@ namespace uSync.MemberEdition.Serializers
 				return member;
 			}
 
-            LogHelper.Warn<MemberHandler>($"Unknown MemberType called {key}");
+			LogHelper.Warn<MemberHandler>($"Unknown MemberType called {key}");
 			return null;
 		});
 
-		private string Filename(IMemberType memberType, IMember member) => 
+		private string Filename(IMemberType memberType, IMember member) =>
 			Umbraco.Core.IO.IOHelper.MapPath(uSyncBackOfficeContext.Instance.Configuration.Settings.Folder + "//" + SyncFolder + "//" + memberType.Alias + "//" + member.Email.ToSafeFileName().Replace('.', '-') + "." + FileExtension);
 
-        public void RegisterEvents()
-        {
+
+		private Cryptography Cryptography(IMember member) => new Cryptography(member.Email + member.Name);
+
+		public void RegisterEvents()
+		{
 			MemberService.Saved += MemberService_Saved;
 			MemberService.Deleted += MemberService_Deleted;
-        }
+		}
 
 		private void MemberService_Saved(IMemberService sender, Umbraco.Core.Events.SaveEventArgs<IMember> e)
 		{
-            if (uSyncEvents.Paused)
+			if (uSyncEvents.Paused)
 			{
-                return;
+				return;
 			}
 			foreach (var member in e.SavedEntities)
 			{
@@ -98,9 +103,9 @@ namespace uSync.MemberEdition.Serializers
 
 		private void MemberService_Deleted(IMemberService sender, Umbraco.Core.Events.DeleteEventArgs<IMember> e)
 		{
-            if (uSyncEvents.Paused)
+			if (uSyncEvents.Paused)
 			{
-                return;
+				return;
 			}
 			foreach (var member in e.DeletedEntities)
 			{
@@ -122,11 +127,11 @@ namespace uSync.MemberEdition.Serializers
 		}
 
 
-		private SyncAttempt<XElement> Serialize(IMemberType memberType, IMember member)
+		private XElement Serialize(IMemberType memberType, IMember member)
 		{
-			var node = new XElement(NodeName, 
+			var node = new XElement(NodeName,
 				new XAttribute(KeyAttribute, member.Email),
-                new XAttribute(TypeAttribute, memberType.Alias),
+				new XAttribute(TypeAttribute, memberType.Alias),
 				new XAttribute(NameAttribute, member.Name),
 				new XAttribute(UserAttribute, member.Username)
 			);
@@ -138,9 +143,14 @@ namespace uSync.MemberEdition.Serializers
 			node.Add(new XElement(LastLockedOutDateNode, member.LastLockoutDate));
 			node.Add(new XElement(LastLoginDateNode, member.LastLoginDate));
 			node.Add(new XElement(LastPasswordChangeDateNode, member.LastPasswordChangeDate));
-			node.Add(new XElement(PasswordQuestionNode, member.PasswordQuestion));
-			node.Add(new XElement(RawPasswordAnswerNode, member.RawPasswordAnswerValue));
-
+			if (!string.IsNullOrWhiteSpace(member.PasswordQuestion))
+			{
+				node.Add(new XElement(PasswordQuestionNode, member.PasswordQuestion));
+			}
+			if (!string.IsNullOrWhiteSpace(member.RawPasswordAnswerValue))
+			{
+				node.Add(new XElement(RawPasswordAnswerNode, member.RawPasswordAnswerValue));
+			}
 			var securityGroups = System.Web.Security.Roles.GetRolesForUser(member.Username);
 			if (securityGroups.Any())
 			{
@@ -155,16 +165,16 @@ namespace uSync.MemberEdition.Serializers
 			var memberDto = Database.SingleOrDefault<MemberDto>(member.Id);
 			if (memberDto != null && !string.IsNullOrWhiteSpace(memberDto.Password))
 			{
-				node.Add(new XElement(PasswordNode, memberDto.Password));
+				var cryptography = Cryptography(member);
+				node.Add(new XElement(PasswordNode, cryptography.Encrypt(memberDto.Password)));
 			}
 
-            foreach (var prop in member.Properties.Where(p => p != null && !p.Alias.StartsWith("umbracoMember")))
+			foreach (var prop in member.Properties.Where(p => p != null && !p.Alias.StartsWith("umbracoMember")))
 			{
 				node.Add(CreateProperty(prop));
 			}
-		
-			return SyncAttempt<XElement>.Succeed(member.Email, node, typeof(IMember), ChangeType.Export);
 
+			return node.Normalize();
 		}
 
 		private uSyncAction ExportMember(IMemberType memberType, IMember member)
@@ -175,22 +185,23 @@ namespace uSync.MemberEdition.Serializers
 			}
 
 			try
-            {
-				var attempt = Serialize(memberType, member);
-                var filename = Filename(memberType, member);
-                uSyncIOHelper.SaveNode(attempt.Item, filename);
-                return uSyncActionHelper<XElement>.SetAction(attempt, filename);
+			{
+				var node = Serialize(memberType, member);
+				var attempt = SyncAttempt<XElement>.Succeed(member.Email, node, typeof(IMember), ChangeType.Export);
+				var filename = Filename(memberType, member);
+				uSyncIOHelper.SaveNode(attempt.Item, filename);
+				return uSyncActionHelper<XElement>.SetAction(attempt, filename);
 			}
-            catch(Exception ex)
-            {
-                LogHelper.Warn<MemberHandler>($"Error saving Member {member.Email}: {ex.Message}");
-                return uSyncAction.Fail(member.Email, typeof(IMember), ChangeType.Export, ex);
-            }
+			catch (Exception ex)
+			{
+				LogHelper.Warn<MemberHandler>($"Error saving Member {member.Email}: {ex.Message}");
+				return uSyncAction.Fail(member.Email, typeof(IMember), ChangeType.Export, ex);
+			}
 		}
 
-        private IEnumerable<uSyncAction> ExportFolder(IMemberType memberType)
-        {
-            var actions = new List<uSyncAction>();
+		private IEnumerable<uSyncAction> ExportFolder(IMemberType memberType)
+		{
+			var actions = new List<uSyncAction>();
 			foreach (var member in _memberService.GetMembersByMemberType(memberType.Id))
 			{
 				actions.Add(ExportMember(memberType, member));
@@ -201,7 +212,7 @@ namespace uSync.MemberEdition.Serializers
 
 		public IEnumerable<uSyncAction> ExportAll(string folder)
 		{
-            var actions = new List<uSyncAction>();
+			var actions = new List<uSyncAction>();
 			foreach (var memberType in _memberTypeService.GetAll())
 			{
 				actions.AddRange(ExportFolder(memberType));
@@ -209,54 +220,54 @@ namespace uSync.MemberEdition.Serializers
 			return actions;
 		}
 
-        private string GetImportIds(PropertyType propType, string content)
-        {
-            var mapping = uSyncCoreContext.Instance.Configuration.Settings.ContentMappings
-                .SingleOrDefault(x => x.EditorAlias == propType.PropertyEditorAlias);
+		private string GetImportIds(PropertyType propType, string content)
+		{
+			var mapping = uSyncCoreContext.Instance.Configuration.Settings.ContentMappings
+				.SingleOrDefault(x => x.EditorAlias == propType.PropertyEditorAlias);
 
-            if (mapping != null)
-            {
-                LogHelper.Debug<Events>("Mapping Content Import: {0} {1}", () => mapping.EditorAlias, () => mapping.MappingType);
+			if (mapping != null)
+			{
+				LogHelper.Debug<Events>("Mapping Content Import: {0} {1}", () => mapping.EditorAlias, () => mapping.MappingType);
 
-                IContentMapper mapper = ContentMapperFactory.GetMapper(mapping);
+				IContentMapper mapper = ContentMapperFactory.GetMapper(mapping);
 
-                if (mapper != null)
-                {
-                    return mapper.GetImportValue(propType.DataTypeDefinitionId, content);
-                }
-            }
+				if (mapper != null)
+				{
+					return mapper.GetImportValue(propType.DataTypeDefinitionId, content);
+				}
+			}
 
-            return content;
-        }
+			return content;
+		}
 
-        private string GetImportXml(XElement parent)
-        {
-            var reader = parent.CreateReader();
-            reader.MoveToContent();
-            string xml = reader.ReadInnerXml();
+		private string GetImportXml(XElement parent)
+		{
+			var reader = parent.CreateReader();
+			reader.MoveToContent();
+			string xml = reader.ReadInnerXml();
 
-            if (xml.StartsWith("<![CDATA["))
-                return parent.Value;
-            else
-                return xml.Replace("&amp;", "&");
-        }
+			if (xml.StartsWith("<![CDATA["))
+				return parent.Value;
+			else
+				return xml.Replace("&amp;", "&");
+		}
 
-		private bool? Deserialize(XElement node, bool force, out IMember newMember)
+		private bool? Deserialize(XElement node, bool force, out IMember member)
 		{
 			bool isNewMember = false;
 			var email = node.Attribute(KeyAttribute);
 			var name = node.Attribute(NameAttribute);
 			var user = node.Attribute(UserAttribute);
 			var memberType = node.Attribute(TypeAttribute);
-			newMember = null;
+			member = null;
 
 			if (email == null || name == null || user == null || memberType == null)
 			{
-                LogHelper.Warn<MemberHandler>($"Error reading {node.Document.BaseUri}");
+				LogHelper.Warn<MemberHandler>($"Error reading {node.Document.BaseUri}");
 				return null;
-			}				
+			}
 
-			var member = _memberService.GetByEmail(email.Value);
+			member = _memberService.GetByEmail(email.Value);
 
 			if (member == null)
 			{
@@ -301,7 +312,7 @@ namespace uSync.MemberEdition.Serializers
 						member.LastLoginDate = DateTime.Parse(el.Value);
 						break;
 
-					case LastPasswordChangeDateNode: 
+					case LastPasswordChangeDateNode:
 						member.LastPasswordChangeDate = DateTime.Parse(el.Value);
 						break;
 
@@ -318,10 +329,8 @@ namespace uSync.MemberEdition.Serializers
 						break;
 
 					default:
-						var alias = el.Attribute(AliasAttribute).Value;
-						var propType = el.Attribute(TypeAttribute).Value;
-
-						var value = GetImportIds(member.Properties[propType].PropertyType, GetImportXml(el));
+						var alias = el.Name.LocalName;
+						var value = GetImportIds(member.Properties[alias].PropertyType, GetImportXml(el));
 						member.SetValue(alias, value);
 						break;
 				}
@@ -363,12 +372,13 @@ namespace uSync.MemberEdition.Serializers
 				var memberDto = Database.SingleOrDefault<MemberDto>(member.Id);
 				if (memberDto != null)
 				{
-					memberDto.Password = password;
+					var cryptography = Cryptography(member);
+					memberDto.Password = cryptography.Decrypt(password);
 					Database.Update(memberDto);
 				}
 				else
 				{
-	                LogHelper.Warn<MemberHandler>($"Member {member.Id} doesn\'t exist in table cmsMember even after we have just saved it");
+					LogHelper.Warn<MemberHandler>($"Member {member.Id} doesn\'t exist in table cmsMember even after we have just saved it");
 				}
 			}
 
@@ -377,10 +387,10 @@ namespace uSync.MemberEdition.Serializers
 
 		private IEnumerable<uSyncAction> ImportFile(string file, bool force)
 		{
-            var actions = new List<uSyncAction>();
-            var xml = XElement.Load(file);
+			var actions = new List<uSyncAction>();
+			var xml = XDocument.Load(file);
 
-			foreach (var el in xml.Nodes().Where(x => x.NodeType == System.Xml.XmlNodeType.Element).Cast<XElement>())
+			foreach (var el in xml.Nodes().Where(x => x.NodeType == System.Xml.XmlNodeType.Element).Cast<XElement>().Where(x => x.Name == NodeName))
 			{
 				IMember member;
 				var success = Deserialize(el, force, out member);
@@ -402,30 +412,30 @@ namespace uSync.MemberEdition.Serializers
 
 		private IEnumerable<uSyncAction> ImportFolder(string folder, bool force)
 		{
-            var actions = new List<uSyncAction>();
-            if (Directory.Exists(folder))
-            {
-                foreach(var file in Directory.GetFiles(folder, FileExtension))
-                {
-                    actions.AddRange(ImportFile(file, force));
-                }
+			var actions = new List<uSyncAction>();
+			if (Directory.Exists(folder))
+			{
+				foreach (var file in Directory.GetFiles(folder, FileFilter))
+				{
+					actions.AddRange(ImportFile(file, force));
+				}
 
-                foreach(var child in Directory.GetDirectories(folder))
-                {
-                    actions.AddRange(ImportFolder(child, force));
-                }
+				foreach (var child in Directory.GetDirectories(folder))
+				{
+					actions.AddRange(ImportFolder(child, force));
+				}
 
-            }
+			}
 
-            return actions;
+			return actions;
 		}
 
 		public IEnumerable<uSyncAction> ImportAll(string folder, bool force)
 		{
-            return ImportFolder(Umbraco.Core.IO.IOHelper.MapPath(folder), force);
+			return ImportFolder(Umbraco.Core.IO.IOHelper.MapPath(folder), force);
 		}
 
-		// Return True, if the elment matches an existing member with same properties, false if they don't match or don't exist, null if we can't import this because of different types
+		// Return True, if the element matches an existing member with same properties, false if they don't match or don't exist, null if we can't import this because of different types
 		private bool? CompareMember(XElement node)
 		{
 			var email = node.Attribute(KeyAttribute);
@@ -435,9 +445,9 @@ namespace uSync.MemberEdition.Serializers
 
 			if (email == null || name == null || user == null || memberType == null)
 			{
-                LogHelper.Warn<MemberHandler>($"Error reading {node.Document.BaseUri}");
+				LogHelper.Warn<MemberHandler>($"Error reading {node.ToString()}");
 				return null;
-			}				
+			}
 
 			var existingMember = _memberService.GetByEmail(email.Value);
 
@@ -446,67 +456,46 @@ namespace uSync.MemberEdition.Serializers
 				return false;
 			}
 
-			foreach (var el in node.Elements())
-			{
-				var alias = el.Attribute(AliasAttribute).Value;
-				var propType = el.Attribute(TypeAttribute).Value;
+			var compare = Serialize(existingMember.ContentType, existingMember);
 
-				var match = existingMember.Properties.FirstOrDefault(x => x.Alias == alias);
-				if (match == null)
-				{
-					return false;
-				}
-
-				if (match.PropertyType.Alias != propType)
-				{
-					return null;	//	We can't convert property types
-				}
-
-				var newProperty = CreateProperty(match);
-				if (newProperty.Value != el.Value)
-				{
-					return false;
-				}
-			}
-
-			return true;
+			return XNode.DeepEquals(node.Normalize(), compare);
 		}
 
-        public IEnumerable<uSyncAction> ReportFile(string file)
-        {
-            var actions = new List<uSyncAction>();
-            var xml = XElement.Load(file);
+		public IEnumerable<uSyncAction> ReportFile(string file)
+		{
+			var actions = new List<uSyncAction>();
+			var xml = XDocument.Load(file);
 
-			foreach (var el in xml.Nodes().Where(x => x.NodeType == System.Xml.XmlNodeType.Element).Cast<XElement>())
+			foreach (var el in xml.Nodes().Where(x => x.NodeType == System.Xml.XmlNodeType.Element).Cast<XElement>().Where(x => x.Name == NodeName))
 			{
-			    actions.Add(uSyncActionHelper<IMember>.ReportAction(CompareMember(el) == false, el.Attribute(KeyAttribute).Value));
+				actions.Add(uSyncActionHelper<IMember>.ReportAction(CompareMember(el) == false, el.Attribute(KeyAttribute).Value));
 			}
 			return actions;
 		}
 
 		public IEnumerable<uSyncAction> ReportFolder(string folder)
-        {
-            var actions = new List<uSyncAction>();
-            if (Directory.Exists(folder))
-            {
-                foreach(var file in Directory.GetFiles(folder, FileExtension))
-                {
-                    actions.AddRange(ReportFile(file));
-                }
+		{
+			var actions = new List<uSyncAction>();
+			if (Directory.Exists(folder))
+			{
+				foreach (var file in Directory.GetFiles(folder, FileFilter))
+				{
+					actions.AddRange(ReportFile(file));
+				}
 
-                foreach(var child in Directory.GetDirectories(folder))
-                {
-                    actions.AddRange(ReportFolder(child));
-                }
+				foreach (var child in Directory.GetDirectories(folder))
+				{
+					actions.AddRange(ReportFolder(child));
+				}
 
-            }
+			}
 
-            return actions;
+			return actions;
 		}
 
 		public IEnumerable<uSyncAction> Report(string folder)
-        {
-            return ReportFolder(Umbraco.Core.IO.IOHelper.MapPath(folder));
-        }
-    }
+		{
+			return ReportFolder(Umbraco.Core.IO.IOHelper.MapPath(folder));
+		}
+	}
 }
---
+++ src/uSync.MemberEdition/Xml/XmlExtensions.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace uSync.MemberEdition.Xml
+{
+	public static class XmlExtensions
+	{
+		private static class Xsi
+		{
+			public static XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+
+			public static XName schemaLocation = xsi + "schemaLocation";
+			public static XName noNamespaceSchemaLocation = xsi + "noNamespaceSchemaLocation";
+		}
+
+		private static IEnumerable<XAttribute> NormalizeAttributes(XElement element, bool havePSVI = false)
+		{
+			return element.Attributes()
+				.Where(a => !a.IsNamespaceDeclaration &&
+					a.Name != Xsi.schemaLocation &&
+					a.Name != Xsi.noNamespaceSchemaLocation)
+				.OrderBy(a => a.Name.NamespaceName)
+				.ThenBy(a => a.Name.LocalName)
+				.Select(
+					a =>
+					{
+						if (havePSVI)
+						{
+							var dt = a.GetSchemaInfo().SchemaType.TypeCode;
+							switch (dt)
+							{
+								case XmlTypeCode.Boolean:
+									return new XAttribute(a.Name, (bool)a);
+								case XmlTypeCode.DateTime:
+									return new XAttribute(a.Name, (DateTime)a);
+								case XmlTypeCode.Decimal:
+									return new XAttribute(a.Name, (decimal)a);
+								case XmlTypeCode.Double:
+									return new XAttribute(a.Name, (double)a);
+								case XmlTypeCode.Float:
+									return new XAttribute(a.Name, (float)a);
+								case XmlTypeCode.HexBinary:
+								case XmlTypeCode.Language:
+									return new XAttribute(a.Name,
+										((string)a).ToLower());
+							}
+						}
+						return a;
+					}
+				);
+		}
+
+		public static XNode Normalize(this XNode node, bool havePSVI = false)
+		{
+			// trim comments and processing instructions from normalized tree
+			if (node is XComment || node is XProcessingInstruction)
+				return null;
+			XElement e = node as XElement;
+			if (e != null)
+				return Normalize(e, havePSVI);
+			// Only thing left is XCData and XText, so clone them
+			return node;
+		}
+
+		public static XElement Normalize(this XElement element, bool havePSVI = false)
+		{
+			if (havePSVI)
+			{
+				var dt = element.GetSchemaInfo();
+				switch (dt.SchemaType.TypeCode)
+				{
+					case XmlTypeCode.Boolean:
+						return new XElement(element.Name,
+							NormalizeAttributes(element, havePSVI),
+							(bool)element);
+					case XmlTypeCode.DateTime:
+						return new XElement(element.Name,
+							NormalizeAttributes(element, havePSVI),
+							(DateTime)element);
+					case XmlTypeCode.Decimal:
+						return new XElement(element.Name,
+							NormalizeAttributes(element, havePSVI),
+							(decimal)element);
+					case XmlTypeCode.Double:
+						return new XElement(element.Name,
+							NormalizeAttributes(element, havePSVI),
+							(double)element);
+					case XmlTypeCode.Float:
+						return new XElement(element.Name,
+							NormalizeAttributes(element, havePSVI),
+							(float)element);
+					case XmlTypeCode.HexBinary:
+					case XmlTypeCode.Language:
+						return new XElement(element.Name,
+							NormalizeAttributes(element, havePSVI),
+							((string)element).ToLower());
+					default:
+						return new XElement(element.Name,
+							NormalizeAttributes(element, havePSVI),
+							element.Nodes().Select(n => Normalize(n, havePSVI))
+						);
+				}
+			}
+			else
+			{
+				return new XElement(element.Name,
+					NormalizeAttributes(element, havePSVI),
+					element.Nodes().Select(n => Normalize(n, havePSVI))
+				);
+			}
+		}
+	}
+}
